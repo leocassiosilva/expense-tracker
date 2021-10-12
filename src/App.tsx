3e716c406ffbdd9b33a955e908cecbd5e31d36be
leocassiosilva <leocassiosilva1234@gmail.com>
2021-10-12 13:36:42 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles'
+import {Item} from './types/Item';
+import {Category} from './types/Category';
+import {categories} from './data/categories';
+import {items} from './data/items';
+import { useState, useEffect } from 'react';
+import {getCurrentMonth, filterListByMonth} from './helpers/dateFilter'
+import {TableArea} from './components/TableArea';
+const App = () => {
 
-function App() {
+  const [list,setList] = useState(items);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+
+  useEffect(()=>{
+    setFilteredList(filterListByMonth(list, currentMonth));
+  },[list, currentMonth]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Finaceiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/*area de informações*/ }
+        {/*area de inserção de informação*/ }
+        <TableArea list={filteredList}/>
+      </C.Body>
+    </C.Container>
   );
 }
 
---
+++ src/components/TableArea/index.tsx
@@ -0,0 +1,30 @@
+import * as C from './styles';
+import {Item} from '../../types/Item';
+
+type Props = {
+    list:Item[]
+}
+export const TableArea = ({ list }: Props) =>{
+    return (   
+        <C.Table>
+            <thead>
+                <tr>
+                    <C.TableHeadColumn width={100}>Data</C.TableHeadColumn>
+                    <C.TableHeadColumn width={130}>Categoria</C.TableHeadColumn>
+                    <C.TableHeadColumn>Titulo</C.TableHeadColumn>
+                    <C.TableHeadColumn width={150}>Valor</C.TableHeadColumn>
+                </tr>
+            </thead>
+            <tbody>
+                {list.map((item,index)=>(
+                    <tr key={index}>
+                        <td>{item.date}</td>
+                        <td>{item.category}</td>
+                        <td>{item.title}</td>
+                        <td>{item.value}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </C.Table>
+    );
+}
